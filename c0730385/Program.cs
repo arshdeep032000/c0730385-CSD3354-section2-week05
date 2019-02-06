6af9d209ad8c2e62e68c995fc14df788aabe9a27
visualstudio <visualstudio@122-PC29>
2019-02-06 10:15:12 -0500

    k

--- c0730385/Program.cs
+++ c0730385/Program.cs
@@ -5,11 +5,31 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace c0730385
+
 {
-    class Program
+    class arshdeep
     {
+        static int i = 0;
         static void Main(string[] args)
         {
+            MethodA();
         }
+        public static void MethodA()
+        {
+            while (peanut())
+            {
+                Console.WriteLine(i);
+                if (i > 10)
+                {
+                    return;
+                }
+            }
+        }
+        public static bool peanut()
+        {
+            i++;
+            return true;
+        }
+
     }
 }
